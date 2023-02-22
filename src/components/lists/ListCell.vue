c22aa3a10aa32bcd3017170968c429a2636cbfd6
asahi-fukuda <asahi.fukuda@myu-on.co.jp>
2023-02-22 13:30:50 +0900

    画面の実装

---
+++ src/components/lists/ListCell.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+.list-cell
+  .time 
+    div {{ time }}
+  .message
+    div {{ message }}
+  .name 
+    div {{ name }}
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export default defineComponent({
+  props: {
+    time: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    message: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    name: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.list-cell {
+  border-bottom: solid 2px whitesmoke;
+  background-color: #fff;
+  line-height: 15px;
+  &:first-child {
+    border-top: solid 2px whitesmoke;
+  }
+  .time {
+    margin-left: 15px;
+    font-size: 12px;
+    text-align: left;
+  }
+  .message {
+    margin-left: 15px;
+    font-size: 18px;
+    text-align: left;
+    font-weight: bold;
+  }
+  .name {
+    margin-right: 5px;
+    font-size: 12px;
+    text-align: right;
+  }
+}
+</style>
---
+++ src/components/modals/Modal.vue
@@ -0,0 +1,113 @@
+<template lang="pug">
+.modal-area(v-show="isActive")
+  .modal-bg(@click="close")
+  .modal-wrapper
+    .modal-content
+      .modal-label
+        label Name
+      .modal-form
+        input(v-model="name", type="text")
+      .modal-label
+        label Message
+      .modal-form
+        input.input(v-model="message", type="text")
+      .buttons
+        button.cancel(@click="close") CANCEL
+        SpinnerButton(text="POST" @click="click2" ref="spinnerButton")
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import SpinnerButton from '@/components/buttons/SpinnerButton.vue'
+export default defineComponent({
+  components: {
+    SpinnerButton,
+  },
+  setup() {
+    const spinnerButton = ref<InstanceType<typeof SpinnerButton>>()
+    const name = ref('')
+    const message = ref('')
+    const isActive = ref(false)
+    const open = () => {
+      isActive.value = true
+    }
+    const close = () => {
+      isActive.value = false
+    }
+    const click2 = () => {
+      spinnerButton.value?.progress()
+      // spinnerButton.value?.onCompleted()
+    }
+    return {
+      click2,
+      spinnerButton,
+      name,
+      message,
+      isActive,
+      open,
+      close,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.modal-area {
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.modal-bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(30, 30, 30, 0.9);
+}
+.button {
+  width: 68%;
+  text-align: right;
+}
+.list {
+  width: 60%;
+  margin: auto;
+}
+.modal-wrapper {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 70%;
+  max-width: 500px;
+  padding: 40px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+.modal-label {
+  text-align: left;
+  label {
+    font-weight: bold;
+  }
+}
+.modal-form {
+  margin-bottom: 20px;
+  input {
+    width: 95%;
+    height: 25px;
+  }
+}
+.buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+.cancel {
+  border: none;
+  margin-right: 35px;
+  cursor: pointer;
+  padding: 12px 30px;
+  background-color: #fff;
+  font-weight: bold;
+}
+</style>
